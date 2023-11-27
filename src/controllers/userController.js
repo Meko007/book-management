6f826b582fb7ac0282dc918e1ddfff0089b23cea
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-27 23:32:57 +0100

    login route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,11 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 //@desc Register user
 //@route GET /api/users
 //@access public
-export const registerUser = async (req,res) => {
+export const registerUser = async (req, res) => {
     try{
         const { username, email, password } = req.body;
         const userExists = await User.findOne({ email });
@@ -24,6 +25,30 @@ export const registerUser = async (req,res) => {
             username: user.username, 
             email: user.email 
         });
+    }catch(err){
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if(user && (await bcrypt.compare(password, user.password))){
+            const accessToken = jwt.sign({
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id
+                },
+            }, process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "1m" }
+            );
+            res.status(200).json({ accessToken });
+        }else{
+            res.status(401);
+            throw new Error('Email or password is invalid');
+        }
     }catch(err){
         res.status(500).json({ message: err.message });
     }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import {
     registerUser,
-    // currentUser,
-    // loginUser
+    loginUser,
+    // currentUser
 } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 
-// router.post('/login', loginUser);
+router.post('/login', loginUser);
 
 // router.get('/current', currentUser);
 
