41879ecc9c50eb93b524a519b5298824998b50f9
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-28 21:20:38 +0100

    cleaned up a few variables

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import router from './src/routes/bookRoute.js';
-import userRouter from './src/routes/userRoute.js';
+import bookRoutes from './src/routes/bookRoute.js';
+import userRoutes from './src/routes/userRoute.js';
 
 dotenv.config();
 const app = express();
@@ -11,15 +11,15 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/books", router);
-app.use("/api/users", userRouter);
+app.use("/api/books", bookRoutes);
+app.use("/api/users", userRoutes);
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Ndeewo');
-})
+});
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
-})
+});
