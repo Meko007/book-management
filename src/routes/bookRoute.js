1f773b359ea587f71418a8146a2dd4cd90452f57
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-28 22:24:54 +0100

    cleaning up

--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -10,15 +10,6 @@ import { validateToken } from '../middleware/validate.js';
 
 const router = express.Router();
 
-// export default router.post("/", async (req, res) => {
-//     const book = new bookModel(req.body);
-//     try{
-//         await book.save();
-//         res.send(book);
-//     }catch(err){
-//         response.status(500).send(err);
-//     }
-// });
 router.use(validateToken);
 router.route('/').get(getBooks).post(postBook);
 router.route('/:id').get(getBook).put(updateBook).delete(deleteBook);
