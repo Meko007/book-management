b962e83f86ef68e6a3a4b8d8b1d32818b4170d45
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-06 01:56:26 +0100

    restricted deletion of books to only admins

--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -6,12 +6,12 @@ import {
     updateBook,
     deleteBook
 } from '../controllers/bookController.js';
-import { validateToken } from '../middleware/validate.js';
+import { validateToken, restrict } from '../middleware/validate.js';
 
 const router = express.Router();
 
 router.use(validateToken);
 router.route('/').get(getBooks).post(postBook);
-router.route('/:id').get(getBook).put(updateBook).delete(deleteBook);
+router.route('/:id').get(getBook).put(updateBook).delete(restrict('admin'), deleteBook);
 
 export default router;
