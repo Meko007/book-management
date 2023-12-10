3103699af21fa81aaf2833bfdcc277428565fdff
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-10 22:20:54 +0100

    cors addition

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import bookRoutes from './src/routes/bookRoute.js';
@@ -9,6 +10,7 @@ const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/books", bookRoutes);
