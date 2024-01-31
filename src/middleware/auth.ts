7b7715b184f5eceb53789d08d7cc4da6ff47568f
Okem Onyeka <jesse007onyeka@gmail.com>
2024-01-31 02:24:56 +0100

    added checks for admin and suspended accounts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,12 +5,17 @@ import 'dotenv/config';
 
 const secret = process.env.JWT_SECRET as string;
 
-interface customReq extends Request {
+export interface customReq extends Request {
     token: string | JwtPayload;
 }
 
 export const createToken = (user: IUser) => {
-	const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '365d' });
+	const token = jwt.sign({
+		userId: user._id,
+		email: user.email,
+		role: user.role, 
+		suspended: user.suspended,
+	}, secret, { expiresIn: '365d' });
 	return token;
 };
 
@@ -22,11 +27,43 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
 			return res.status(401).json({ message: 'Unauthorized' });
 		}
         
-		const decodedToken = jwt.verify(token, secret) as JwtPayload;
-		(req as customReq).token = { userId: decodedToken.userId };
+		const decodedToken = jwt.verify(token, secret);
+		(req as customReq).token = decodedToken;
 		next();
 	} catch (error) {
-		res.status(401).json({ message: 'Unauthorized' });
+		res.status(500).json({ message: 'Internal Server Error' });
+		console.error(error);
+	}
+};
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const user = (req as customReq).token;
+	
+		// if (user && (user as JwtPayload).role === 'admin') {
+		// 	next();
+		// } else {
+		// 	res.status(403)
+		// }
+	
+		user && (user as JwtPayload).role === 'admin'
+			? next()
+			: res.status(403).json({ message: 'Admin access required' });
+	} catch (error) {
+		res.status(500).json({ message: 'Internal Server Error' });
+		console.error(error);
+	}
+};
+
+export const isSuspended = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const user = (req as customReq).token;
+
+		user && (user as JwtPayload).suspended
+			? res.status(403).json({ message: 'Your account has been suspended' })
+			: next();
+	} catch (error) {
+		res.status(500).json({ message: 'Internal Server Error' });
 		console.error(error);
 	}
 };
