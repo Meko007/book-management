659bef04948c59bd5b212a901933df5eac0ecfb6
Okem Onyeka <jesse007onyeka@gmail.com>
2024-02-01 20:33:21 +0100

    added three new routes

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -5,6 +5,9 @@ import {
 	getUsers,
 	deleteUser,
 	updateUser,
+	logoutUser,
+	forgotPassword,
+	resetPassword,
 } from '../controllers/userController';
 import { verifyToken, isAdmin } from '../middleware/auth';
 
@@ -12,7 +15,10 @@ const router = express.Router();
 
 router.post('/users/register', registerUser);
 router.post('/users/login', loginUser);
+router.post('/users/logout', logoutUser);
 router.get('/users', verifyToken, isAdmin, getUsers);
+router.post('/users/forgot-password', forgotPassword);
+router.post('/users/reset-password', resetPassword);
 router.route('/users/:id')
 	.put(verifyToken, isAdmin, updateUser)
 	.delete(verifyToken, isAdmin, deleteUser);
