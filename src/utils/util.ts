19a9d70095fa8a82538bdd46c1cce0172017bdd4
Okem Onyeka <jesse007onyeka@gmail.com>
2024-02-01 20:32:24 +0100

    added a few more helper functions

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,6 @@
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
+
 export const random = (length: number): string => {
 	let result = '';
 	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -10,5 +13,28 @@ export const random = (length: number): string => {
 export const isEmail = (email: string): boolean => 
 	(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email);
 
-export const capitalizeName = (name: string): string => 
-	name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+export const capitalizeName = (name: string): string => {
+	return name
+		.split(/\s+/)
+		.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+		.join(' ');
+};
+
+// Sanitize text inputs
+export const sanitizeText = (text: string): boolean => /^[A-Za-z]+$/.test(text);
+export const sanitizeText1 = (text: string): boolean => 
+	/^[A-Za-z]+(?:\s[A-Za-z]+)*$/.test(text);
+export const sanitizeText2 = (text: string): boolean => 
+	/^[A-Za-z\s\d\W]+(?:\s[A-Za-z\s\d\W]+)*$/.test(text);
+
+// NODEMAILER
+export const emailAddress = process.env.EMAIL as string;
+export const emailPassword = process.env.EMAIL_PASS as string;
+
+export const transporter = nodemailer.createTransport({
+	service: 'Gmail',
+	auth: {
+		user: emailAddress,
+		pass: emailPassword,
+	},
+});
