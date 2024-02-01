b92a90a65c3f041f8adea2c986b713d1ec1abe70
Okem Onyeka <jesse007onyeka@gmail.com>
2024-02-01 21:53:53 +0100

    validating inputs, favouriting books

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from 'express';
 import BookModel, { IBook } from '../models/bookModel';
+import UserModel from '../models/userModel';
+import { validationResult, check } from 'express-validator';
+import { customReq } from '../middleware/auth';
+import { JwtPayload } from 'jsonwebtoken';
 
 export const postBook = async (req: Request, res: Response) => {
 	try {
-		const { title, author, genre, pubYear, price, picURL } = req.body;
-		const titleExists = await BookModel.findOne({ title: { $regex: new RegExp(title, 'i') } });
+		await Promise.all([
+			check('title').isString().trim().escape().notEmpty().run(req),
+			check('author').isString().trim().escape().notEmpty().run(req),
+			check('genre').isArray().trim().escape().notEmpty().run(req),
+			check('pubYear').isNumeric().run(req),
+			check('bookURL').isString().trim().notEmpty().run(req),
+			check('picURL').isString().trim().notEmpty().run(req),
+		]);
+
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { title, author, genre, pubYear, bookURL, picURL } = req.body;
+		const titleExists = await BookModel.findOne({ title: { $regex: new RegExp(`^${title}$`, 'i') } });
 
 		if (titleExists) {
 			return res.status(409).json({ message: `${title} already exists` });
@@ -15,8 +33,10 @@ export const postBook = async (req: Request, res: Response) => {
 			author,
 			genre,
 			pubYear,
-			price,
-			picURL
+			bookURL,
+			picURL,
+			favouritesCount: 0,
+			favouritedBy: [],
 		});
 		res.status(201).json(newBook);
 	} catch (error) {
@@ -27,7 +47,7 @@ export const postBook = async (req: Request, res: Response) => {
 
 export const getBooks = async (req: Request, res: Response) => {
 	try {
-		const books = await BookModel.find(req.query, '-_id -createdAt -updatedAt -__v');
+		const books = await BookModel.find(req.query, '-_id -favouritedBy -createdAt -updatedAt -__v');
 		res.status(200).json(books);
 	} catch (error) {
 		res.status(500).json({ error: 'Internal Server Error' });
@@ -68,6 +88,80 @@ export const deleteBook = async (req: Request, res: Response) => {
 			message: 'deleted',
 			book
 		});
+	} catch (error) {
+		res.status(500).json({ error: 'Internal Server Error' });
+		console.error(error);
+	}
+};
+
+export const favouriteBook = async (req: Request, res: Response) => {
+	try {
+		const { bookId } = req.params;
+		const userId = ((req as customReq).token as JwtPayload).userId;
+		const book = await BookModel.findById(bookId);
+
+		if (!book) {
+			return res.status(404).json({ message: 'Book not found' });
+		}
+
+		const user = await UserModel.findById(userId);
+
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+
+		if (book.favouritedBy.includes(userId)) {
+			return res.status(409).json({ message: 'You\'ve favourited this book' });
+		}
+
+		if (user.favourites.includes(bookId)) {
+			return res.status(400).json({ message: 'Book already in favourites' });
+		}
+
+		await BookModel.findByIdAndUpdate(book._id, {
+			$inc: { 'favouritesCount': 1 }, 
+			$push: { 'favouritedBy': userId } 
+		}, { new: true });
+		await UserModel.findByIdAndUpdate(user._id, { $push: { 'favourites': bookId } }, { new: true });
+
+		res.status(200).json({ message: 'added to your favourites' });
+	} catch (error) {
+		res.status(500).json({ error: 'Internal Server Error' });
+		console.error(error);
+	}
+};
+
+export const removeBookFromFavourites = async (req: Request, res: Response) => {
+	try {
+		const { bookId } = req.params;
+		const userId = ((req as customReq).token as JwtPayload).userId;
+		const book = await BookModel.findById(bookId);
+
+		if (!book) {
+			return res.status(404).json({ message: 'Book not found' });
+		}
+
+		const user = await UserModel.findById(userId);
+
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+
+		if (!book.favouritedBy.includes(userId)) {
+			return res.status(409).json({ message: 'You didn\'t favourite this book' });
+		}
+
+		if (!user.favourites.includes(bookId)) {
+			return res.status(400).json({ message: 'Book not in favourites' });
+		}
+
+		await BookModel.findByIdAndUpdate(book._id, {
+			$inc: { 'favouritesCount': -1 }, 
+			$pull: { 'favouritedBy': userId } 
+		});
+		await UserModel.findByIdAndUpdate(user._id, { $pull: { 'favourites': bookId } });
+
+		res.status(200).json({ message: 'removed from your favourites' });
 	} catch (error) {
 		res.status(500).json({ error: 'Internal Server Error' });
 		console.error(error);
